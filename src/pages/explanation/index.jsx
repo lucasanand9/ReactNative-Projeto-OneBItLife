339a5a2986849c081f81dd3921d5c0ded93d2946
lucasanand9 <lucasanand9@gmail.com>
2022-11-30 17:30:46 -0300

    criando a estrutura da pagina Explanation

---
+++ src/pages/explanation/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import DefaultButton from "../../components/commun/DefaultButton";
+
+export default function Explanation(){
+    function handleSetShowHome(){
+        console.log("testando botao")
+    }
+
+    return(
+        <View style={Styles.container}>
+            <ScrollView>
+                <View style={{alignItems: "center"}}>
+                    <Text style={Styles.title}>Antes, deixa {"\n"} eu te explicar...</Text>
+                    <Text style={Styles.descriptionCta}>
+                        Pronto para subir de nivel na vida?
+                    </Text>
+                    <Text style={Styles.description}>
+                        Na proxima tela voce vai poder escolher {'\n'} seus 4 hábitos de forma individual
+                    </Text>   
+
+                    <DefaultButton
+                    buttonText={"Continuar"}
+                    handlePress={handleSetShowHome}
+                    width={250}
+                    height={100}
+                    />             
+                </View>
+            </ScrollView>
+        </View>
+
+    );
+
+}
+
+const Styles = StyleSheet.create({
+
+    container:{
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    title:{
+        fontSize:30,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: 'center',
+        marginVertical: 40,
+    },
+    descriptionCta:{
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16,
+        marginTop: 20,
+        marginBottom: 10,
+       },
+    description:{
+        color: "white",
+        textAlign: "center",
+        marginBottom: 30,
+    },
+})
